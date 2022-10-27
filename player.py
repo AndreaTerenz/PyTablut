53638695b92bc91f52cf2f9f0f4f35f8d6aa248a
andrea <andrea.terenziani.at@gmail.com>
2022-10-27 16:07:16 +0200

    Refactoring (primo di tanti)

--- player.py
+++ player.py
@@ -18,6 +18,20 @@ class BasePlayer(ABC):
 
         self.board.send_move(move)
 
+    def check_move(self, move_r, move_c, cell_r, cell_c):
+        cell_type = self.board.grid[move_r, move_c].type
+        checker = self.board.grid[move_r, move_c].checker
+
+        if self.role == "WHITE":
+            return checker == CheckerType.EMPTY and cell_type in [CellType.NORMAL, CellType.ESCAPE]
+        elif self.role == "BLACK":
+            if self.board.grid[cell_r, cell_c].type == CellType.CAMP:
+                # Deve essere VUOTA
+                # Non puo' essere il CASTLE
+                return checker == CheckerType.EMPTY and cell_type != CellType.CASTLE
+            else:
+                return checker == CheckerType.EMPTY and cell_type in [CellType.NORMAL, CellType.ESCAPE]
+
     @abstractmethod
     def play_black(self):
         pass
@@ -27,11 +41,10 @@ class BasePlayer(ABC):
         pass
 
 class RandomPlayer(BasePlayer):
-    def play_random(self, check_fn):
+    def play_random(self):
         """
         Play a random move given your role
 
-        :param check_fn: returns whether a certain move is valid
         :return: a move
         """
 
@@ -45,25 +58,25 @@ class RandomPlayer(BasePlayer):
 
         # Explore left
         for j in range(c-1,-1,-1):
-            if check_fn(r,j,r,c):
+            if self.check_move(r,j,r,c):
                 moves.append((r,j))
             else:
                 break
         # Explore right
         for j in range(c + 1, 9):
-            if check_fn(r,j,r,c):
+            if self.check_move(r,j,r,c):
                 moves.append((r,j))
             else:
                 break
         # Explore up
         for i in range(r - 1, -1, -1):
-            if check_fn(i,c,r,c):
+            if self.check_move(i,c,r,c):
                 moves.append((i,c))
             else:
                 break
         # Explore down
         for i in range(r + 1, 9):
-            if check_fn(i,c,r,c):
+            if self.check_move(i,c,r,c):
                 moves.append((i,c))
             else:
                 break
@@ -71,26 +84,7 @@ class RandomPlayer(BasePlayer):
         return ic(rnd.choice(moves))
 
     def play_black(self):
-        # Scegli pedina bianca a caso X
-        # Prendi lista mosse possibili per X
-        # Scegli a caso mossa per X
-        def check_cell_black(move_r, move_c, cell_r, cell_c):
-            cell_type = self.board.grid[move_r, move_c].type
-            checker = self.board.grid[move_r, move_c].checker
-
-            if self.board.grid[cell_r, cell_c].type == CellType.CAMP:
-                # Deve essere VUOTA
-                # Non puo' essere il CASTLE
-                return checker == CheckerType.EMPTY and cell_type != CellType.CASTLE
-            else:
-                return checker == CheckerType.EMPTY and cell_type in [CellType.NORMAL, CellType.ESCAPE]
-
-        return self.play_random(check_cell_black)
+        return self.play_random()
 
     def play_white(self):
-        def check_cell_white(move_r, move_c, cell_r, cell_c):
-            cell_type = self.board.grid[move_r, move_c].type
-            checker = self.board.grid[move_r, move_c].checker
-            return checker == CheckerType.EMPTY and cell_type in [CellType.NORMAL, CellType.ESCAPE]
-
-        return self.play_random(check_cell_white)
+        return self.play_random()
