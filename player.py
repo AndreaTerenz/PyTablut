d9a25486a42261455ef673875c8edc1d002b471e
andrea <andrea.terenziani.at@gmail.com>
2022-10-29 18:34:29 +0200

    Fixed bug where random player wouldn't try another checker if the first one didn't have any avaliable moves

--- player.py
+++ player.py
@@ -94,41 +94,54 @@ class RandomPlayer(BasePlayer):
         """
 
         # Get the list of your checkers
-        other_checkers = self.board.whites if self.role == "WHITE" else self.board.blacks
-        random_checker = (rnd.choice(other_checkers))
-        r = random_checker[0]
-        c = random_checker[1]
-        ic(r,c)
-        moves = []
+        your_checkers = self.board.whites if self.role == "WHITE" else self.board.blacks
+        # shuffle your checkers in a random order
+        rnd.shuffle(your_checkers)
 
-        # Explore left
-        for j in range(c-1,-1,-1):
-            if self.check_move(r,j,r,c):
-                moves.append((r,j))
-            else:
-                break
-        # Explore right
-        for j in range(c + 1, 9):
-            if self.check_move(r,j,r,c):
-                moves.append((r,j))
-            else:
-                break
-        # Explore up
-        for i in range(r - 1, -1, -1):
-            if self.check_move(i,c,r,c):
-                moves.append((i,c))
-            else:
-                break
-        # Explore down
-        for i in range(r + 1, 9):
-            if self.check_move(i,c,r,c):
-                moves.append((i,c))
-            else:
-                break
+        # We loop over the checkers - as soon as we have one
+        # with at least one legal move, we return
+        for random_checker in your_checkers:
+            r = random_checker[0]
+            c = random_checker[1]
+            ic(r,c)
+            moves = []
 
-        #FIXME: THIS LIST COULD BE EMPTY! ops
-        ic(moves)
-        return ic(rnd.choice(moves))
+            # Explore left
+            for j in range(c-1,-1,-1):
+                if self.check_move(r,j,r,c):
+                    moves.append((r,j))
+                else:
+                    break
+            # Explore right
+            for j in range(c + 1, 9):
+                if self.check_move(r,j,r,c):
+                    moves.append((r,j))
+                else:
+                    break
+            # Explore up
+            for i in range(r - 1, -1, -1):
+                if self.check_move(i,c,r,c):
+                    moves.append((i,c))
+                else:
+                    break
+            # Explore down
+            for i in range(r + 1, 9):
+                if self.check_move(i,c,r,c):
+                    moves.append((i,c))
+                else:
+                    break
+
+            ic(moves)
+
+            if len(moves) > 0:
+                output = rnd.choice(moves)
+                return ic(output)
+            else:
+                ic("NO MOVES FOUND")
+
+        # If we get here, there were no legal moves for any checker
+        #FIXME: THE FUCK DO WE DO IN THIS CASE?
+        ic("If you see me there's a big problem")
 
     def play_black(self):
         return self.play_random()
