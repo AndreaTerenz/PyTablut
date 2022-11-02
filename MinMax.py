fe848c8099123f06e9a27554ee387034aafa5ac9
francesco-source <frencypiv@gmail.com>
2022-11-02 16:43:14 +0100

    Streetking

--- MinMax.py
+++ MinMax.py
@@ -1,29 +1,39 @@
 from types import CellType
-import  numpy as np
+import numpy as np
 import math 
 from player import BasePlayer
 from board import Board, CheckerType,CellType
 
 
+def StreetKing(player):
+    king=player.board.king
+    for i in player.board.grid[:,king[1]]:
+        if i.checker!=CheckerType.EMPTY or i.checker!=CheckerType.KING:
+            return False
+    for i in player.board.grid[king[0],:]:
+        if i.checker!=CheckerType.EMPTY or i.checker!=CheckerType.KING:
+            return False
+    return True    
+
 def Children(board):
     #deve un vettore con le Board figlie di Board
     return None
 
-def Heuristic(board,node):
-    king=board.king
-    if node=="WHITE":
-        escapes=board.grid[CellType.ESCAPE,CheckerType.EMPTY]
+def Heuristic(player):
+    king=player.board.king
+    if player.role=="WHITE":
+        escapes=player.board.grid[CellType.ESCAPE,CheckerType.EMPTY]
         distances=np.array([np.linalg.norm(king-i,2) for i in escapes])
         min_distance=int(np.min(distances))
         NeatenEnemies=...#lo ritorna la funzione della Chiara
         return int(min_distance+NeatenEnemies)
-    elif node=="BLACK":
+    elif player.role=="BLACK":
         NeatenEnemies=...# lo ritorna la funzione della Chiara
         king_eaten=...# lo ritorna la  funzione della Chiara
         if king_eaten==True:
             return -np.inf
         else:
-            return NeatenEnemies     
+            return -NeatenEnemies     
     #l heuristica deve ritornare un max se sei un bianco
     #il max sarà la distanza in R2 del re dalla strada di casa
     #deve avere un peso positivo se il posizionamento d una pedina bianca mangia una nera
@@ -36,7 +46,7 @@ def Heuristic(board,node):
 def minmax(position,depth,alpha=-np.inf,beta=+np.inf,maximizingPlayer=None):
     #funziona in caso di un tree in cui ogni ogni nodo due figli, 
     #devo pensare se funziona anche con un nodo con più figli
-    #ma penso di si
+    #ma penso di si, poco efficiente però.
     if depth==0 :
         return Heuristic(position)
     if maximizingPlayer=="WHITE":
