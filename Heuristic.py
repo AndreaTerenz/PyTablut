6cda74e08fab1c980e53b9b6941e5616704097a3
francesco-source <frencypiv@gmail.com>
2022-11-07 21:44:18 +0100

    Tablut with aima

--- Heuristic.py
+++ Heuristic.py
@@ -29,28 +29,42 @@ def king_in_danger(player):
     if enemies_in_row != 0 and enemies_in_column != 0:
         return -int(enemies_in_row+enemies_in_column)
     else :
-        return 1
+        return 0
+
+
+    ''' Piu il re è vicino ad un escape piu ha peso la distanza
+    Piu il re è lontano dall'escape piu ha peso il numero di nemici.
     
+    
+    
+    
+    
+    '''
 
 
 def heuristic(player):
     king = player.board.king
     if player.role == "WHITE":
+        if king[0]==100 and king[1]==100:
+            return -np.inf
+    #se il king è nell'escape ritorna piu infinito
         escapes = player.board.grid[CellType.ESCAPE,CheckerType.EMPTY]
         d_to_escapes = np.array([np.linalg.norm(king-i,2) for i in escapes])
         min_d_to_escapes = int(np.min(d_to_escapes))    
+
         Nenemies=len(player.board.blacks)
-        param0=1/min_d_to_escapes
-        param1=1/Nenemies
+
+        param0=9/min_d_to_escapes
+        param1=16-Nenemies
         param2=king_in_danger(player)
-        w0=rd.gauss(0,1)
-        w1=rd.gauss(0,1)
-        w2=rd.gauss(0,1)
-        return param0*w0+param1*w1+param2*w2
+        return param0+param1+param2
     if player.role == "BLACK":
-        w0=rd.gauss(0,1)
-        Nenemies=len(player.board.whites)
-        king_eaten = player.board.king== False
-        if king_eaten==False:
-            return None
-        return w0*Nenemies
+        if king[0]==100 and king[1]==100:
+                return +np.inf
+        blacks=player.board.blacks
+        distances=np.array([np.linalg.norm(king-i,2) for i in blacks])
+        d_mean=int(np.mean(distances))
+        Nenemies = 9-len(player.board.whites)
+        king_eaten = player.board.king == False
+        return Nenemies+d_mean
+
